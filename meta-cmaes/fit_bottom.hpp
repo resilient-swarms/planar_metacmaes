026b3417e7ad65f436ba5bf1ef7138a1ae5ff20c
agb1n19 <58887711+agb1n19@users.noreply.github.com>
2020-08-17 07:33:02 +0100

    Add files via upload

---
+++ meta-cmaes/fit_bottom.hpp
@@ -0,0 +1,291 @@
+#ifndef FITBOTTOM_HPP
+#define FITBOTTOM_HPP
+
+#include <meta-cmaes/feature_vector_typedefs.hpp>
+#include <meta-cmaes/bottom_typedefs.hpp>
+#include <Eigen/Dense>
+
+#if CONTROL()
+
+#include <modules/map_elites/fit_map.hpp>
+#endif
+
+/* bottom-level fitmap 
+used to evaluate behavioural descriptor and fitness of controllers in the normal operating environment
+*/
+
+namespace sferes
+{
+namespace fit
+{
+#if CONTROL() 
+
+SFERES_FITNESS(FitBottom, sferes::fit::FitMap)
+{
+#else
+class FitBottom
+{
+#endif
+
+public:
+    FitBottom() : _dead(false){};
+#if META()
+    weight_t W;
+
+    FitBottom(const weight_t &w) : W(w), _dead(false)
+    {
+    }
+#endif
+    inline void set_fitness(float fFitness)
+    {
+#if CONTROL()
+        this->_objs.resize(1);
+        this->_objs[0] = fFitness;
+#endif
+        this->_value = fFitness;
+    }
+    inline void set_dead(bool dead)
+    {
+        this->_dead = dead;
+    }
+
+    template <typename Indiv>
+    void eval(Indiv & indiv)
+    {
+
+        //std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+        this->_value = 0;
+        this->_dead = false;
+        _eval<Indiv>(indiv);
+        //std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+        // std::cout << "Time difference = " <<     std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+    }
+
+    // override the function so that we can write behaviour descriptor values along with the fitness value
+    template <class Archive>
+    void serialize(Archive & ar, const unsigned int version)
+    {
+        ar &BOOST_SERIALIZATION_NVP(this->_value);
+        ar &BOOST_SERIALIZATION_NVP(this->_desc);
+    }
+
+    bool dead() { return _dead; }
+    std::vector<double> ctrl() { return _ctrl; }
+
+    std::vector<float> get_desc(simulator_t & simu, base_features_t & b)
+    {
+#if NO_WEIGHT()
+        std::vector<double> vec;
+#if POS_C()
+        //std::cout << "pos "<<std::endl;
+        simu.get_descriptor<planar_dart::descriptors::PositionalCoord, std::vector<double>>(vec);
+#elif POL_C()
+        //std::cout << "pol "<<std::endl;
+        simu.get_descriptor<planar_dart::descriptors::PolarCoord, std::vector<double>>(vec);
+#elif RA_C()
+        //std::cout << "RA "<<std::endl;
+        simu.get_descriptor<planar_dart::descriptors::ResultantAngle, std::vector<double>>(vec);
+#elif AS_C()
+        //std::cout << "AS "<<std::endl;
+        simu.get_descriptor<planar_dart::descriptors::AngleSum, std::vector<double>>(vec);
+
+#else
+#error "Please give a viable condition in {0,1,2,3,4}"
+#endif
+        return std::vector<float>(vec.begin(), vec.end());
+#else
+        //get the base_features
+        get_base_features(b, simu);
+        // get descriptor
+        return get_desc(b);
+#endif
+    }
+
+#if WEIGHT()
+    std::vector<float> get_desc(const base_features_t &b)
+    {
+
+#if META()
+        //std::cout << "META "<<std::endl;
+        bottom_features_t D = W * b;
+#elif GLOBAL_WEIGHT()
+        //std::cout << "random "<<std::endl;
+        bottom_features_t D = global::W * b;
+#endif
+        std::vector<float> vec(D.data(), D.data() + D.rows() * D.cols());
+#ifdef PRINTING
+        std::cout << " getting descriptor " << std::endl;
+#if META()
+        std::cout << " w =  " << W << std::endl;
+#elif GLOBAL_WEIGHT()
+        std::cout << " w =  " << global::W << std::endl;
+#endif
+        std::cout << " b = " << b << std::endl;
+        std::cout << " D = " << D << std::endl;
+#endif
+        return vec;
+    }
+#endif
+#if META()
+    template <typename Individual>
+    static void add_to_database(Individual & ind)
+    {
+        if (!ind.fit().dead())
+        {
+            //push to the database
+            global::database.push_back(global::data_entry_t(ind.gen().data(), ind.fit().b(), ind.fit().value()));
+#ifdef PRINTING
+            std::cout << " adding entry with fitness " << ind.fit().value() << std::endl;
+#endif
+        }
+    }
+    base_features_t b()
+    {
+        return _b;
+    }
+    void set_b(const base_features_t &features)
+    {
+        _b = features;
+    }
+    void set_b(const std::vector<float> &vec)
+    {
+        for (size_t i = 0; i < NUM_BASE_FEATURES; ++i)
+        {
+            _b(i, 0) = vec[i];
+        }
+    }
+    mode::mode_t mode() const
+    {
+        return _mode;
+    }
+    void set_mode(mode::mode_t m)
+    {
+        _mode = m;
+    }
+
+    void set_desc(const std::vector<float> &d)
+    {
+        _desc = d;
+    }
+
+    void set_value(float v)
+    {
+        _value = v;
+    }
+    std::vector<float> desc()
+    {
+        return _desc;
+    }
+
+    float value()
+    {
+        return _value;
+    }
+#endif
+
+protected:
+    std::vector<double> _ctrl;
+    bool _dead;
+    base_features_t _b;
+
+#if META() // these are already defined by FitMap
+    mode::mode_t _mode;
+    float _value = 0.0f;
+    std::vector<float> _desc;
+#endif
+
+    // descriptor work done here, in this case duty cycle
+    template <typename Indiv>
+    void _eval(Indiv & indiv)
+    {
+
+        // copy of controller's parameters
+        _ctrl.clear();
+
+        for (size_t i = 0; i < 24; i++)
+            _ctrl.push_back(indiv.data(i));
+
+        // launching the simulation
+        auto robot = global::global_robot->clone();
+
+        simulator_t simu(_ctrl, robot);
+        simu.run();
+
+        set_fitness(simu.performance_val());
+
+        std::vector<float> desc;
+
+        float dead = -1000.0f;
+        // these assume a behaviour descriptor of size 6.
+        if (dead > this->_value)
+        {
+            // this means that something bad happened in the simulation
+            // we kill this individual
+            this->_dead = true; // no need to do anything
+                                // desc.resize(6);
+                                // desc[0] = 0;
+                                // desc[1] = 0;
+                                // desc[2] = 0;
+                                // desc[3] = 0;
+                                // desc[4] = 0;
+                                // desc[5] = 0;
+                                // this->_value = -1000.0f;// note this causes troubles;
+                                // -> due to optimisation (presumably) the code is evaluated within if first, therefore the above condition seems to always be true
+        }
+        else
+        {
+            // convert to final descriptor
+            base_features_t b;
+            this->_desc = get_desc(simu, b);
+            this->_dead = false;
+#if META()
+            set_b(b);
+#endif
+#ifdef PRINTING
+            std::cout << " fitness is " << this->_value << std::endl;
+#endif
+
+        }
+    }
+
+    /* the included descriptors determine the base-features */
+    void get_base_features(base_features_t & base_features, simulator_t & simu)
+    {
+
+        std::vector<double> results;
+        simu.get_descriptor<planar_dart::descriptors::PositionalCoord, std::vector<double>>(results);
+
+        for (size_t i = 0; i < results.size(); ++i)
+        {
+            base_features(i, 0) = results[i];
+        }
+        size_t offset = results.size();
+        simu.get_descriptor<planar_dart::descriptors::PolarCoord, std::vector<double>>(results);
+        for (size_t i = 0; i < results.size(); ++i)
+        {
+            base_features(i + offset, 0) = results[i];
+        }
+
+        offset += results.size();
+
+        simu.get_descriptor<planar_dart::descriptors::ResultantAngle, std::vector<double>>(results);
+        for (size_t i = 0; i < results.size(); ++i)
+        {
+            base_features(i + offset, 0) = results[i];
+        }
+
+        offset += results.size();
+        simu.get_descriptor<planar_dart::descriptors::AngleSum, std::vector<double>>(results);
+        for (size_t i = 0; i < results.size(); ++i)
+        {
+            base_features(i + offset, 0) = results[i];
+        }
+        //std::cout << "W" << std::endl;
+        //std::cout << base_features<< std::endl;
+    }
+};
+} // namespace fit
+} // namespace sferes
+
+#endif
---
+++ meta-cmaes/stat_pop.hpp
@@ -0,0 +1,251 @@
+#ifndef STAT_DATABASE_HPP_
+#define STAT_DATABASE_HPP_
+
+#include <numeric>
+#include <boost/multi_array.hpp>
+#include <boost/serialization/array.hpp>
+#include <sferes/stat/stat.hpp>
+#include <meta-cmaes/bottom_typedefs.hpp>
+#include <meta-cmaes/mapelites_phenotype.hpp>
+#include <meta-cmaes/top_typedefs.hpp>
+#include <meta-cmaes/stat_map.hpp>
+#include <Eigen/Dense>
+
+#include <chrono>
+typedef std::chrono::high_resolution_clock Clock;
+
+// #define MAP_WRITE_PARENTS
+
+namespace boost
+{
+template <class Archive, typename _Scalar, int _Rows, int _Cols, int _Options, int _MaxRows, int _MaxCols>
+inline void serialize(
+    Archive &ar,
+    Eigen::Matrix<_Scalar, _Rows, _Cols, _Options, _MaxRows, _MaxCols> &t,
+    const unsigned int file_version)
+{
+    size_t rows = t.rows(), cols = t.cols();
+    ar &rows;
+    ar &cols;
+    if (rows * cols != t.size())
+        t.resize(rows, cols);
+
+    for (size_t i = 0; i < t.size(); i++)
+        ar &BOOST_SERIALIZATION_NVP(t.data()[i]);
+}
+} // namespace boost
+
+namespace sferes
+{
+namespace stat
+{
+SFERES_STAT(Stat_Pop, Stat)
+{
+
+public:
+    std::vector<boost::shared_ptr<Phen>> _pop;
+    size_t _capacity, _sp, _max_sp;
+    global::database_t _database;
+    size_t _nb_evals;
+    template <typename E>
+    void refresh(const E &ea)
+    {
+
+        if (ea.gen() % CMAESParams::pop::dump_period == 0)
+        {
+#ifdef PRINTING
+            std::cout << "starting dump of Stat_Database" << std::endl;
+            _write_database(std::string("database_"), ea);
+#endif
+            _pop = ea.pop();
+            _write_recovered_performances(std::string("recovered_perf"), ea);
+            get_database();
+            std::cout << "number of evaluations so far " << _nb_evals << std::endl;
+        }
+    }
+
+    void get_database()
+    {
+        _capacity = global::database.get_capacity();
+        _database = global::database;
+        _sp = global::database.sp;
+        _max_sp = global::database.max_sp;
+        _nb_evals = global::nb_evals;
+    }
+    void set_globals()
+    {
+        // reset the data-base
+        global::database = _database;
+        global::database.sp = _sp;
+        global::database.max_sp = _max_sp;
+        global::nb_evals = _nb_evals;
+    }
+    // show the n-th individual from zero'th map in the population
+    void show(std::ostream & os, size_t k)
+    {
+
+        auto t1 = Clock::now();
+
+        for (size_t i = 0; i < this->_pop.size(); ++i)
+        {
+            this->_pop[i]->develop(); // don't take into account any additions to the database; so you know which one is the best according to evolution
+            os << this->_pop[i]->W << std::endl;
+            os << "END WEIGHTS" << std::endl;
+        }
+        auto t2 = Clock::now();
+        std::cout << "database load time for 5 maps: "
+                  << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count()
+                  << " milliseconds" << std::endl;
+#ifdef GRAPHIC // we are just interested in observing a particular individual
+        size_t count = 0;
+        std::cout << "loading individual" << k << std::endl;
+        for (const bottom_indiv_t *j = this->_pop[0]->archive().data(); j < (this->_pop[0]->archive().data() + this->_pop[0]->archive().num_elements()); ++j)
+        {
+            if (*j)
+            {
+                if (count == k)
+                {
+                    float val = sferes::fit::RecoveredPerformance<base_phen_t>::_eval_all(**j);
+                    std::cout << val << std::endl;
+                    break;
+                }
+                ++count;
+            }
+            //std::cout << count;
+        }
+
+#else
+        // don't take into account any additions to the database; so you know which one is the best according to evolution
+        for (size_t i = 0; i < this->_pop.size(); ++i)
+        {
+
+            _show_(os, this->_pop[i]->archive());
+        }
+#endif
+    }
+
+    void _show_(std::ostream & os, const boost::multi_array<bottom_indiv_t, BottomParams::ea::behav_dim> &archive)
+    {
+        std::cout << "show stat" << std::endl;
+        std::cout << "read the archive" << std::endl;
+#ifdef INDIVIDUAL_DAMAGE
+        test_max_recovery(os, archive);
+#else
+        test_recoveredperformance(os, archive);
+#endif
+    }
+    void test_recoveredperformance(std::ostream & os, const boost::multi_array<bottom_indiv_t, BottomParams::ea::behav_dim> &archive)
+    {
+        float val = 0.0f;
+
+        for (const bottom_indiv_t *k = archive.data(); k < (archive.data() + archive.num_elements()); ++k)
+        {
+            if (*k)
+            {
+                val = sferes::fit::RecoveredPerformance<base_phen_t>::_eval_all(**k);
+#ifdef EVAL_ENVIR
+                val /= (float)global::world_options.size();
+#else
+                val /= (float)global::damage_sets.size();
+#endif
+                os << val << std::endl;
+            }
+        }
+
+        os << "END TEST META-INDIVIDUAL" << std::endl;
+    }
+    // assess maximal recovery for each damage separately
+    void test_max_recovery(std::ostream & os, const boost::multi_array<bottom_indiv_t, BottomParams::ea::behav_dim> &archive)
+    {
+
+        std::vector<bottom_indiv_t> individuals;
+        for (const bottom_indiv_t *k = archive.data(); k < (archive.data() + archive.num_elements()); ++k)
+        {
+            if (*k)
+            {
+                individuals.push_back(*k);
+            }
+        }
+	sferes::fit::RecoveredPerformance<base_phen_t>::_eval_taskmax(os,individuals);
+
+    }
+    template <class Archive>
+    void serialize(Archive & ar, const unsigned int version)
+    {
+        // reset the data-base
+        ar &BOOST_SERIALIZATION_NVP(_pop);
+        ar &BOOST_SERIALIZATION_NVP(_capacity);
+
+        for (size_t i = 0; i < _capacity; ++i)
+        {
+            ar &BOOST_SERIALIZATION_NVP(_database[i]);
+        }
+        ar &BOOST_SERIALIZATION_NVP(_sp);
+        ar &BOOST_SERIALIZATION_NVP(_max_sp);
+        ar &BOOST_SERIALIZATION_NVP(_nb_evals);
+        if (Archive::is_loading::value)
+        {
+            set_globals();
+        }
+    }
+
+protected:
+    template <typename EA>
+    void _write_database(const std::string &prefix,
+                         const EA &ea) const
+    {
+        std::cout << "writing..." << ea.gen() << prefix << std::endl;
+        std::string fname = ea.res_dir() + "/" + boost::lexical_cast<std::string>(ea.gen()) + prefix + std::string(".dat");
+
+        std::ofstream ofs(fname.c_str());
+        for (size_t k = 0; k < global::database.size(); ++k)
+        {
+            ofs << global::database[k].base_features << "\t" << global::database[k].fitness << std::endl;
+        }
+    }
+
+    template <typename EA>
+    void _write_recovered_performances(const std::string &prefix,
+                                       const EA &ea) const
+    {
+        std::cout << "writing..." << prefix << ea.gen() << std::endl;
+        std::string fname = ea.res_dir() + "/" + prefix + boost::lexical_cast<std::string>(ea.gen()) + std::string(".dat");
+
+        std::ofstream ofs(fname.c_str());
+        for (size_t k = 0; k < _pop.size(); ++k)
+        {
+            ofs << _pop[k]->fit().value() << "\t";
+        }
+        ofs << "\n";
+    }
+};
+
+} // namespace stat
+} // namespace sferes
+
+// SFERES_STAT(Stat_Pop, Stat)
+// {
+//     typedef boost::shared_ptr<phen_t> indiv_t;
+//     typedef typename std::vector<indiv_t> pop_t;
+//     pop_t _pop;
+//     size_t nb_evals;
+
+// public:
+//     Stat_Pop() {}
+
+//     template <typename E>
+//     void refresh(const E &ea)
+//     {
+//     }
+
+//     template <class Archive>
+//     void serialize(Archive & ar, const unsigned int version)
+//     {
+
+//         ar &BOOST_SERIALIZATION_NVP(_pop);
+//     }
+// };
+//} // namespace stat
+//} // namespace sferes
+
+#endif
