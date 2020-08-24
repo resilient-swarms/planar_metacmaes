444fecab0e0ec729ead03fbd994e351f801e771a
agb1n19 <58887711+agb1n19@users.noreply.github.com>
2020-08-24 10:08:51 +0100

    Update setEnv.sh

--- setEnv.sh
+++ setEnv.sh
@@ -1,15 +1,15 @@
-export RESIBOTS_DIR=/robo
-export BOTS_DIR=/planar_robo
-export SFERES_DIR=/home/agb/sferes2
-export RESULTS_DIR=/planar_robo/results
+export RESIBOTS_DIR=/robo/
+export BOTS_DIR=/planar_robo/
+export SFERES_DIR=/home/agb/sferes2/
+export RESULTS_DIR=/planar_robo/results/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/lib:/usr/lib:/usr/local/lib
 
 cd
 git clone git@github.com:agb1n19/planar_metacmaes.git
-cd ~/planar_metacmaes
+cd ~/planar_metacmaes/
 sh setEnv.sh
 mkdir extras
-cd ~/planar_metacmaes/extras
+cd ~/planar_metacmaes/extras/
 git clone git://github.com/dartsim/dart.git
 cd dart
 git checkout v6.3.0
@@ -29,7 +29,7 @@ git clone https://github.com/resilient-swarms/map_elites.git
 cd $SFERES_DIR
 ./waf configure
 ./waf
-mkdir exp && cd exp
+mkdir exp && cd exp/
 cp -r ~/planar_metacmaes/planar_cmaes/ $SFERES_DIR/exp/
 cd $SFERES_DIR
 export BUILD_META=FALSE
