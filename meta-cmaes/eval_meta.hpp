57f7497891dc65f418206dbf6c882e48d5659557
David <davidmbossens@gmail.com>
2021-01-19 00:39:16 +0000

    make no-damage binary

--- meta-cmaes/eval_meta.hpp
+++ meta-cmaes/eval_meta.hpp
@@ -45,14 +45,19 @@ namespace sferes
             {
                 std::vector<boost::shared_ptr<base_phen_t>> pop = meta_i.sample_individuals(percent);
                 float fit = 0.0f;
+#ifdef NO_DAMAGE_METAFIT
+
+                std::vector<planar_dart::planarDamage> damages = {};
+#else
                 for (size_t j = 0; j < global::damage_sets.size(); ++j)
                 {
                     std::vector<planar_dart::planarDamage> damages = get_damageset(j);
+#endif
                     std::vector<Eigen::VectorXd> positions;
                     for (size_t i = 0; i < pop.size(); ++i)
                     {
                         // evaluate the individual
-                        std::tuple<Eigen::VectorXd, bool> tup = sferes::fit::PairwiseDist<base_phen_t>::_eval_single_envir(*pop[i],damages);
+                        std::tuple<Eigen::VectorXd, bool> tup = sferes::fit::PairwiseDist<base_phen_t>::_eval_single_envir(*pop[i], damages);
                         bool dead = std::get<1>(tup);
                         if (!dead)
                         {
@@ -61,75 +66,87 @@ namespace sferes
                         // use dead information for database?
                     }
                     fit += sferes::fit::PairwiseDist<base_phen_t>::compute(positions);
+#ifdef NO_DAMAGE_METAFIT
+                    value = fit;
+                    nb_evals = pop.size();
+#else
                 }
                 /* result */
                 value = fit / (float)global::damage_sets.size();
                 nb_evals = global::damage_sets.size() * pop.size();
+#endif
             }
-        };
+            }; // namespace eval
 
-        template <typename Phen>
-        struct _eval_parallel_meta : public _eval_parallel_individuals<CSharedMemPosition, base_phen_t, bottom_fit_t>
-        {
-            float value;
-            size_t nb_evals;
-            Phen meta_indiv;
-            std::vector<planar_dart::planarDamage> damages;
-            _eval_parallel_meta(Phen &meta_i, float percent)
-            { //now join the bottom-level fitnesses
-                this->_pop = meta_i.sample_individuals();
-                value = 0.0f;
-                for (size_t j = 0; j < global::damage_sets.size(); j += 2)
-                {
-                    damages = get_damageset(j);
-                    this->run();
-                }
-                /* result */
-                value = value / (float)global::damage_sets.size();
-                nb_evals = global::damage_sets.size() * this->_pop.size();
-            }
-            virtual void LaunchSlave(size_t slave_id)
+            template <typename Phen>
+            struct _eval_parallel_meta : public _eval_parallel_individuals<CSharedMemPosition, base_phen_t, bottom_fit_t>
             {
-                // we sample directly from map of developed individuals, so the individuals do not need to be developed, nor does their fitness prototype be set
-                std::tuple<Eigen::VectorXd, bool> tup = sferes::fit::PairwiseDist<base_phen_t>::_eval_single_envir(*this->_pop[slave_id], damages);
-                // write fitness and death value (no descriptors) to shared memory
-                dynamic_cast<CSharedMemPosition *>(shared_memory[slave_id])->setPosition(std::get<0>(tup)); // ASSUME SINGLE OBJECTIVE
-                dynamic_cast<CSharedMemPosition *>(shared_memory[slave_id])->setDeath(std::get<1>(tup));    // no need
+                float value;
+                size_t nb_evals;
+                Phen meta_indiv;
+                std::vector<planar_dart::planarDamage> damages;
+                _eval_parallel_meta(Phen &meta_i, float percent)
+                { //now join the bottom-level fitnesses
+                    this->_pop = meta_i.sample_individuals();
+                    value = 0.0f;
+#ifdef NO_DAMAGE_METAFIT
+                    damages = {};
+                    this->run();
+                    /* result */
+                    nb_evals = this->_pop.size();
+#else
+                    for (size_t j = 0; j < global::damage_sets.size(); j += 2)
+                    {
+                        damages = get_damageset(j);
+                        this->run();
+                    }
+                    /* result */
+                    value = value / (float)global::damage_sets.size();
+                    nb_evals = global::damage_sets.size() * this->_pop.size();
+#endif
+                }
+                virtual void LaunchSlave(size_t slave_id)
+                {
+                    // we sample directly from map of developed individuals, so the individuals do not need to be developed, nor does their fitness prototype be set
+                    std::tuple<Eigen::VectorXd, bool> tup = sferes::fit::PairwiseDist<base_phen_t>::_eval_single_envir(*this->_pop[slave_id], damages);
+                    // write fitness and death value (no descriptors) to shared memory
+                    dynamic_cast<CSharedMemPosition *>(shared_memory[slave_id])->setPosition(std::get<0>(tup)); // ASSUME SINGLE OBJECTIVE
+                    dynamic_cast<CSharedMemPosition *>(shared_memory[slave_id])->setDeath(std::get<1>(tup));    // no need
 
 #ifdef CHECK_PARALLEL
-                std::cout << "child position " << slave_id << " " << std::get<0>(tup) << std::endl;
-                std::cout << "child death " << slave_id << " " << std::get<1>(tup) << std::endl;
+                    std::cout << "child position " << slave_id << " " << std::get<0>(tup) << std::endl;
+                    std::cout << "child death " << slave_id << " " << std::get<1>(tup) << std::endl;
 #endif
-                this->quit();
-            }
+                    this->quit();
+                }
 
-            virtual void write_data()
-            {
-                std::vector<Eigen::VectorXd> positions;
-                /* Back in the parent, copy the scores into the population data */
-                for (size_t i = 0; i < this->_pop.size(); ++i)
+                virtual void write_data()
                 {
-
-                    bool dead = shared_memory[i]->getDeath();
-                    Eigen::VectorXd pos = dynamic_cast<CSharedMemPosition *>(shared_memory[i])->getPosition();
-                    if (!dead)
+                    std::vector<Eigen::VectorXd> positions;
+                    /* Back in the parent, copy the scores into the population data */
+                    for (size_t i = 0; i < this->_pop.size(); ++i)
                     {
-                        positions.push_back(pos);
+
+                        bool dead = shared_memory[i]->getDeath();
+                        Eigen::VectorXd pos = dynamic_cast<CSharedMemPosition *>(shared_memory[i])->getPosition();
+                        if (!dead)
+                        {
+                            positions.push_back(pos);
+                        }
+
+                        //Fit::add_metaeval_to_database(*this->_pop[i]);
+
+#ifdef CHECK_PARALLEL
+
+                        std::cout << "parent position " << i << " " << pos << std::endl;
+                        std::cout << "parent death " << i << " " << dead << std::endl;
+#endif
                     }
 
-                    //Fit::add_metaeval_to_database(*this->_pop[i]);
-
-#ifdef CHECK_PARALLEL
-
-                    std::cout << "parent position " << i << " " << pos << std::endl;
-                    std::cout << "parent death " << i << " " << dead << std::endl;
-#endif
+                    value += sferes::fit::PairwiseDist<base_phen_t>::compute(positions);
                 }
-
-                value += sferes::fit::PairwiseDist<base_phen_t>::compute(positions);
-            }
-        };
-    } // namespace eval
-} // namespace sferes
+            };
+        } // namespace sferes
+    }     // namespace eval
 
 #endif
